6ba44028a1842e6e0aaa1fe4eff8439a3a4e9962
dre123456 <dre123456@mail.ru>
2018-11-25 14:13:22 +0300

    исправленно

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirstApp;
+
+namespace FirstApp
+{
+    public class listMeter
+    {
+        public void MeasuringAddingProcessAdd(List<int> lMetter)
+        {
+            Stopwatch ProcessTime = new Stopwatch();
+            ProcessTime.Start();
+            for (int i = 0; i <= 10000000; i++)
+            {
+                lMetter.Add(0);
+            }
+            ProcessTime.Stop();
+            Console.WriteLine(ProcessTime.Elapsed);
+        }      
+    }
+    class Program
+    {
+        static void Main()
+        {
+            List<int> a = new List<int>(new LinkedList<int>());
+            listMeter o1 = new listMeter();
+            System.Console.WriteLine("LinkedList TimeMillisAdd=" + o1.MeasuringAddingProcessAdd(a));
+            /* System.Console.WriteLine("LinkedList TimeMillisGet=" + o1.MeasuringAddingProcessGet(a));
+               System.Console.WriteLine("LinkedList TimeMillisGet=" + o1.MeasuringAddingProcessRemove(a));*/
+            System.Console.WriteLine();
+        }
+    }
+}
