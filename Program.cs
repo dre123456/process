1a00618c535cffd7d10254f36598ff593c51944b
dre123456 <dre123456@mail.ru>
2018-12-09 12:39:32 +0300

    вот придумал с ифам сделать удоление, это правельное решение ?

--- Program.cs
+++ Program.cs
@@ -37,15 +37,17 @@ namespace FirstApp
             long elapsedTime = ts.Milliseconds;
             return elapsedTime;
         }
-            
-               
-                        public long MeasuringAddingProcessRemove(List<int> lMetter)
+
+               public long MeasuringAddingProcessRemove(List<int> lMetter)
                         {
 
                             Stopwatch myStopwatch = new Stopwatch();
                             myStopwatch.Start();
-                            for (int i = 0; i <= 10000000; i++)
-                                lMetter.Remove(0);
+            for (int i = 0; i <= 10000000; i++)
+               if (i == 10000000)
+              {
+                    lMetter.Remove(i);
+               }
                             myStopwatch.Stop();
                             TimeSpan ts = myStopwatch.Elapsed;
                             long elapsedTime = ts.Milliseconds;
@@ -60,7 +62,7 @@ namespace FirstApp
             listMeter o1 = new listMeter();
             System.Console.WriteLine("LinkedList TimeMillisAdd=" + o1.MeasuringAddingProcessAdd(a));
             System.Console.WriteLine("LinkedList TimeMillisGet=" + o1.MeasuringAddingProcessGet(a));
-            System.Console.WriteLine("LinkedList TimeMillisGet=" + o1.MeasuringAddingProcessRemove(a));
+            System.Console.WriteLine("LinkedList TimeMillisRemove=" + o1.MeasuringAddingProcessRemove(a));
             System.Console.ReadKey();
         }
     }
